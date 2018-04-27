4be7314b43d28dac7f196645bc19ede2912cd236
Dominic-Bartlomowicz <dbartlomowicz@gmail.com>
2018-04-27 11:33:06 -0700

    formatting changes

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -12,7 +12,9 @@ def create
   @Category = Category.new(Category_params)
 
 if @Category.save
-  redirect_to[:admin, categories], notice: "The new category has been created!"
+  redirect_to[:admin, :categories], notice: "The new category has been created!"
 else
   render :new
+end
+
 end
