1938e3a300049838c925e881cf6fc641ae882f3e
Dominic-Bartlomowicz <dbartlomowicz@gmail.com>
2018-04-30 13:51:47 -0700

    added authentication to more admin controllers

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with :username => ENV["DATABASE_USERNAME"], :password => ENV["DATABASE_PASSWORD"]
+
   def show
   end
 end
