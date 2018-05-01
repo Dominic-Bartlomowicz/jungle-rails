0ce019be45b5624daf2601c2a42ec502177b2a6e
Dominic-Bartlomowicz <dbartlomowicz@gmail.com>
2018-04-30 18:55:25 -0700

    completed exercise

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+
+# describes validation subcategories of name, price, quantity, and category
+  describe 'Validations' do
+    it 'saves given the validation of present name/price/quantity/category' do
+      @category = Category.new
+      @product = Product.new(:name => "Hipster Glasses",
+                                        :price => 20,
+                                        :quantity => 10,
+                                        :category => @category)
+      @product.save!
+      expect(@product.id).to be_present
+    end
+
+
+    # validates :name, presence: true
+      it 'validates the present name' do
+        @category = Category.new
+        @product = Product.new(:name => nil,
+                               :price => 20,
+                               :quantity => 10,
+                               :category => @category)
+        puts(@product.valid?)
+        expect(@product.valid?).not_to be_present
+      end
+
+
+    # validates :price, presence: true
+      it 'validates the present price' do
+        @category = Category.new
+        @product = Product.new(:name => "Hipster Glasses",
+                               :price => "",
+                               :quantity => 10,
+                               :category => @category)
+        expect(@product.valid?).not_to be_present
+      end
+
+
+    # validates :quantity, presence: true
+      it 'validates the present quantity' do
+        @category = Category.new
+        @product = Product.new(:name => "Hipster Glasses",
+                               :price => 20,
+                               :quantity => nil,
+                               :category => @category)
+        expect(@product.valid?).not_to be_present
+      end
+
+
+    # validates :category, presence: true
+      it 'validates the present category' do
+        @category = Category.new
+        @product = Product.new(:name => "Hipster Glasses",
+                               :price => 20,
+                               :quantity => 10,
+                               :category => nil)
+        expect(@product.valid?).not_to be_present
+      end
+  end
+end
--- spec/product_spec.rb
+++
@@ -1,8 +0,0 @@
-RSpec.describe Product, type: :model do
-  describe 'Validations' do
-    validates :name, presence: true
-    validates :price, presence: true
-    validates :quantity, presence: true
-    validates :category, presence: true
-  end
-end
